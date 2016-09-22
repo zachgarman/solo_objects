1c25302bad3f76b5c7aaf57d362fb3a921168ccb
Zach Garman <zachgarman@Zachs-MacBook-Pro.local>
2016-09-22 13:19:29 -0500

    Add getter and setter

--- objects.js
+++ objects.js
@@ -1,36 +1,43 @@
 var nd = {
   schoolName: 'Notre Dame',
   mascot: 'Fighting Irish',
-  nickName: 'Irish',
   sport: 'football',
   location: {city: 'South Bend', state: 'Indiana'},
   league: 'NCAA',
   nationalChampionships: 11,
   allAmericans: 83,
   conference: 'Independent',
-  greatestCoach: 'Frank Leahy',
-  popularCoach: 'Knute Rockne',
+  coach: 'Knute Rockne',
   colors: ['blue', 'gold'],
   record: '816-300-33',
-  firstYear: '1899'
+  firstYear: '1899',
+  get nickname() {
+    return this.mascot.substring(9,14);
+  },
+  set popCoach(newCoach) {
+    this.coach = newCoach;
+  }
 };
+
+console.log(nd.nickname);
 
 function aboutND (obj) {
   console.log('The ' + obj.schoolName + ' ' + obj.mascot + ', located in '
               + obj.location.city + ', ' + obj.location.state
               + ', have been a staple of the ' + obj.league + ' since '
-              + obj.firstYear + '. The ' + obj.nickName + ' have '
+              + obj.firstYear + '. The ' + obj.nickname + ' have '
               + 'won ' + obj.nationalChampionships + ' consensus '
               + 'National Championships and have produced '
-              + obj.allAmericans + ' consensus All Americans. '
-              + 'The ' + obj.nickName + ' have compiled a record of '
+              + obj.allAmericans + ' consensus All Americans.\n\n'
+              + 'The ' + obj.nickname + ' have compiled a record of '
               + obj.record + ' as an ' + obj.conference + ' member '
               + 'of the ' + obj.league + '. The school colors are '
               + obj.colors[0] + ' and ' + obj.colors[1] + '. The '
-              + 'school\'s most famous coach is ' + obj.popularCoach
-              + ', while many regard ' + obj.greatestCoach + ' to '
-              + 'be the greatest coach. There is no doubt that '
-              + 'the ' + obj.mascot + ' are the most historic '
+              + 'school\'s most famous coach is ' + obj.coach + '.\n');
+  nd.popCoach = 'Frank Leahy';
+  console.log('Many people regard ' + obj.coach + ' to '
+              + 'be the greatest coach. Either way,there is no doubt '
+              + 'that the ' + obj.mascot + ' are the most historic '
               + obj.sport + ' team in the nation.');
 
 }
